7b82f2d4fa27a1a77f5d748321861e7c04cd3e59
dogukanoksuz <me@dogukan.dev>
2023-03-09 10:01:43 +0300

    feature: JSON parse format

--- handlers/filter.hpp
+++ handlers/filter.hpp
@@ -1,53 +1,138 @@
-#include <pistache/common.h>
-#include <pistache/cookie.h>
+/**
+ * @file filter.hpp
+ * @brief FilterHandler class definition
+ * @author Doğukan Öksüz
+ */
+
 #include <pistache/endpoint.h>
 #include <pistache/http.h>
-#include <pistache/http_headers.h>
-#include <pistache/net.h>
-#include <pistache/peer.h>
+#include <pistache/router.h>
+#include <rapidjson/document.h>
+#include <rapidjson/writer.h>
+#include <rapidjson/stringbuffer.h>
+#include <curl/curl.h>
+#include <algorithm>
 
 using namespace Pistache;
 
-class FilterHandler : public Http::Handler
+class FilterHandler
 {
-    HTTP_PROTOTYPE(FilterHandler)
-
-    void onRequest(
-        const Http::Request& req,
-        Http::ResponseWriter response) override
+public:
+    explicit FilterHandler(Address addr)
+        : httpEndpoint(std::make_shared<Http::Endpoint>(addr))
     {
-        if (req.resource() == "/filter") {
-            if (req.method() == Http::Method::Get) {
-                using namespace Http;
+    }
 
-                //const auto& query = req.query();
+    void init(size_t thr = 2)
+    {
+        auto opts = Http::Endpoint::options()
+                        .threads(static_cast<int>(thr));
+        httpEndpoint->init(opts);
+        setupRoutes();
+    }
 
-                response.send(Http::Code::Ok, "PONG");
-            }
-        }
-        else if (req.resource() == "/echo")
+    void start()
+    {
+        httpEndpoint->setHandler(router.handler());
+        httpEndpoint->serve();
+    }
+
+private:
+    void setupRoutes()
+    {
+        using namespace Rest;
+
+        Routes::Get(router, "/filter", Routes::bind(&FilterHandler::filter, this));
+    }
+
+    void unescape_url(std::string &url)
+    {
+        CURL *curl = curl_easy_init();
+        if (curl)
         {
-            if (req.method() == Http::Method::Post)
-            {
-                response.send(Http::Code::Ok, req.body(), MIME(Text, Plain));
-            }
-            else
-            {
-                response.send(Http::Code::Method_Not_Allowed);
-            }
-        }
-        else
-        {
-            response.send(Http::Code::Not_Found);
+            char *output = curl_easy_unescape(curl, url.c_str(), url.length(), NULL);
+            url = output;
+            curl_free(output);
+            curl_easy_cleanup(curl);
         }
     }
 
-    void onTimeout(
-        const Http::Request& /*req*/,
-        Http::ResponseWriter response) override
+    void filter(const Rest::Request &request, Http::ResponseWriter response)
     {
-        response
-            .send(Http::Code::Request_Timeout, "Timeout")
-            .then([=](ssize_t) {}, PrintException());
+        auto query = request.query();
+        auto filter = query.get("filter");
+
+        if (filter)
+        {
+            auto filter_string = *std::move(filter);
+            unescape_url(filter_string);
+            const char *unparsed_json = filter_string.c_str();
+
+            rapidjson::Document document;
+            document.Parse(unparsed_json);
+
+            if (document.HasParseError())
+            {
+                response.send(Http::Code::Bad_Request, "Invalid JSON");
+                return;
+            }
+
+            std::string parsed_value = "";
+            if (document.IsArray())
+            {
+                for (auto &v : document.GetArray())
+                {
+                    if (v.IsObject())
+                    {
+                        std::string filter_value = "";
+
+                        for (auto &m : v.GetObject())
+                        {
+                            if (strcmp(m.name.GetString(), "key") == 0)
+                            {
+                                filter_value += m.value.GetString();
+                                filter_value += ": ";
+                                continue;
+                            }
+
+                            if (strcmp(m.name.GetString(), "value") == 0)
+                            {
+                                if (m.value.IsString())
+                                {
+                                    filter_value += m.value.GetString();
+                                    filter_value += "\n";
+                                }
+
+                                if (m.value.IsArray())
+                                {
+                                    filter_value += "[\n";
+                                    for (auto &n : m.value.GetArray())
+                                    {
+                                        if (n.IsString())
+                                        {
+                                            filter_value += "\t";
+                                            filter_value += n.GetString();
+                                            filter_value += ",";
+                                            filter_value += "\n";
+                                        }
+                                    }
+                                    filter_value += "]\n";
+                                }
+                            }
+                        }
+
+                        parsed_value += filter_value;
+                        std::cout << filter_value << std::endl;
+                    }
+                }
+            }
+
+            response.send(Http::Code::Ok, parsed_value);
+            return;
+        }
+        response.send(Http::Code::Bad_Request, "No filter provided");
     }
+
+    std::shared_ptr<Http::Endpoint> httpEndpoint;
+    Rest::Router router;
 };
--- main.cpp
+++ main.cpp
@@ -1,77 +1,12 @@
-#include <pistache/common.h>
-#include <pistache/cookie.h>
-#include <pistache/endpoint.h>
-#include <pistache/http.h>
-#include <pistache/http_headers.h>
-#include <pistache/net.h>
-#include <pistache/peer.h>
+/**
+ * @file main.cpp   
+ * @brief Main file for the FilterHandler example
+ * @author Doğukan Öksüz
+*/
+
 #include "handlers/filter.hpp"
 
 using namespace Pistache;
-
-struct LoadMonitor
-{
-    LoadMonitor(const std::shared_ptr<Http::Endpoint>& endpoint)
-        : endpoint_(endpoint)
-        , interval(std::chrono::seconds(1))
-    { }
-
-    void setInterval(std::chrono::seconds secs)
-    {
-        interval = secs;
-    }
-
-    void start()
-    {
-        shutdown_ = false;
-        thread    = std::make_unique<std::thread>([this] { run(); });
-    }
-
-    void shutdown()
-    {
-        shutdown_ = true;
-    }
-
-    ~LoadMonitor()
-    {
-        shutdown_ = true;
-        if (thread)
-            thread->join();
-    }
-
-private:
-    std::shared_ptr<Http::Endpoint> endpoint_;
-    std::unique_ptr<std::thread> thread;
-    std::chrono::seconds interval;
-
-    std::atomic<bool> shutdown_;
-
-    void run()
-    {
-        Tcp::Listener::Load old;
-        while (!shutdown_)
-        {
-            if (!endpoint_->isBound())
-                continue;
-
-            endpoint_->requestLoad(old).then([&](const Tcp::Listener::Load& load) {
-                old = load;
-
-                double global = load.global;
-                if (global > 100)
-                    global = 100;
-
-                if (global > 1)
-                    std::cout << "Global load is " << global << "%" << std::endl;
-                else
-                    std::cout << "Global load is 0%" << std::endl;
-            },
-                                             Async::NoExcept);
-
-            std::this_thread::sleep_for(std::chrono::seconds(interval));
-        }
-    }
-};
 
 int main(int argc, char* argv[])
 {
@@ -92,11 +27,8 @@ int main(int argc, char* argv[])
     std::cout << "Cores = " << hardware_concurrency() << std::endl;
     std::cout << "Using " << thr << " threads" << std::endl;
 
-    auto server = std::make_shared<Http::Endpoint>(addr);
+    FilterHandler handler(addr);
 
-    auto opts = Http::Endpoint::options()
-                    .threads(thr);
-    server->init(opts);
-    server->setHandler(Http::make_handler<FilterHandler>());
-    server->serve();
+    handler.init(thr);
+    handler.start();
 }
